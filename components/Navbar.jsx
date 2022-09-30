afab9c6a3acb1f1130fb8773a9b3cf02b6fb2d11
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-30 23:57:31 +0530

    12th-commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,33 @@
-import Link from 'next/link';
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function Header() {
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="flex flex-col text-center rounded-xl">
-          <Link href="/">
-            <img src="../logo.png" alt="logo" width="120px" className='mx-auto rounded-lg cursor-pointer opacity-100' />
+        <div className="md:float-left block">
+        <Link href="/">
+            <img src="../logo.png" alt="logo" width="69px" className='mx-auto rounded-lg cursor-pointer opacity-100' />
           </Link>
-          {/* <Link href="/">
-              <span className=" font-bold text-5xl  text-pink-300">Blog</span>
-            </Link> */}
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-xl text-pink-100 ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
