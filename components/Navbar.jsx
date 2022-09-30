f3333657538b042b8521924caffe34d52b898452
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-30 23:36:44 +0530

    5th-commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,15 +15,13 @@ const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
+        <div className="flex flex-col text-center rounded-xl">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-5xl  text-pink-300">Blog</span>
+            <img src="../logo.png" alt="logo" width="120px" className='mx-auto rounded-lg cursor-pointer opacity-100' />
           </Link>
-        </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-xl text-pink-100 ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
-          ))}
+          {/* <Link href="/">
+            <span className=" font-bold text-5xl  text-pink-300">Blog</span>
+          </Link> */}
         </div>
       </div>
     </div>
