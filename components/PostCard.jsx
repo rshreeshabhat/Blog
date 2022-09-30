b3d0667f94ccbd8f3659ee376a37004a1c126b23
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-30 18:55:20 +0530

    3rd-commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
-  <div className="bg-pink-100 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+  <div className="bg-blue-100 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
--- components/index.jsx
+++ components/index.jsx
@@ -1,11 +1,11 @@
 export { default as PostCard } from './PostCard';
-export { default as PostDetail } from './PostDetail';
+export { default as PostDetail } from './PostData';
 export { default as Layout } from './Layout';
-export { default as Categories } from './Categories';
+export { default as Categories } from './Category';
 export { default as Author } from './Author';
 export { default as PostWidget } from './PostWidget';
-export { default as AdjacentPostCard } from './AdjacentPostCard';
-export { default as FeaturedPostCard } from './FeaturedPostCard';
-export { default as Comments } from './Comments';
+export { default as AdjacentPostCard } from './NextPostCard';
+export { default as FeaturedPostCard } from './Featured';
+export { default as Comments } from './Comment';
 export { default as CommentsForm } from './CommentsForm';
 export { default as Loader } from './Loader';
